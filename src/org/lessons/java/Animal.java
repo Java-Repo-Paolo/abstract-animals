43858e2a73ddd481e41799e977673e31f9106488
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-25 14:25:58 +0200

    Commit 1 - add abstract class Animal

--- src/org/lessons/java/Animal.java
+++ src/org/lessons/java/Animal.java
@@ -1,2 +1,25 @@
-package org.lessons.java;public class Animal {
+package org.lessons.java;
+
+public abstract class Animal {
+        private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void sleep(){
+        System.out.println("Zzzzzzzz");
+    }
+
+    public abstract void eat();
+
+    public abstract void call();
 }
