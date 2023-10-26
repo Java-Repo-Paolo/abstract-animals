dc5db131a22afa038c4fc591dcdb1de33cf1c16a
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-26 14:03:41 +0200

    Commit 8 - Bonus add creator

--- src/org/lessons/java/bonus/Creator.java
+++ src/org/lessons/java/bonus/Creator.java
@@ -1,8 +1,18 @@
 package org.lessons.java.bonus;
 
-/*public class Creator {
+public class Creator {
 
-    /*public Device create(String type) {
+    public Device create(String type) throws IllegalArgumentException {
+// type può essere o dvd player o playstation
+        switch (type) {
+            case "dvd player":
+                return new DVDelayer();
+            case "playstation":
+                return new Playstation();
+            case "switch":
 
+            default:
+                throw new IllegalArgumentException("Invalid type");
+        }
     }
 }
